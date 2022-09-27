53baf9ae16535b0ad1d6a464be44dec56e9c1f06
Oalvarez <oalvarez500@gmail.com>
2022-09-27 10:02:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const boton = document.getElementById("ingresar");
+boton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  document.getElementById("username").value == "" ||
+    document.getElementById("password").value == "";
+
+  if (username == "" || password == "") alert("Usuario o contraseña vacío");
+  alert("Usuario o contraseña vacio");
+  if (username == "Omar123" && password == "1234") {
+    alert("Bienvenido");
+  } else {
+    document.querySelector("#mensaje").style.display = "block";
+    setTimeout(() => {
+      document.querySelector("#mensaje");
+      style.display = "none";
+    }, 3000);
+  }
+  document.getElementById("password").addEventListener("mouseenter", mostrar);
+
+  function mostrar() {
+    document.getElementById("password").type = "text";
+  }
+  document.getElementById("password").addEventListener("mouseleave", ocultar);
+
+  function ocultar() {
+    document.getElementById("password").type = "password";
+  }
+  document
+    .getElementById("rememberme")
+    .addEventListener("change", function (z) {
+      if (z.target.checked == true) {
+        alert("Desea que recordemos sus datos");
+      }
+    });
+});
